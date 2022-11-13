d7cfe9adcf8c7e833eaa56ab62a3e40c678a4ab4
FelixPolkanov <felixpolkanov@gmail.com>
2022-11-13 22:46:05 +0200

    task-11

--- src/news-service.js
+++ src/news-service.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default class newsApiService {
     constructor() {
         this.searchQuery = '';
@@ -7,16 +9,23 @@ export default class newsApiService {
 
     async fetchArticles() {
         console.log(this.searchQuery);
-        
+    try {
         const fetchData = await fetch(`https://pixabay.com/api/?key=31251439-64cf22bfdeb9633faeca9a5f6&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
         const data = await fetchData.json()
+        
+        if (!fetchData.ok) {
+            throw new Error('Error')
+        }
        
-                console.log('data', data)
-                console.log('data.hits', data.hits)
+            console.log('data', data)
+            console.log('data.hits', data.hits)
                 
-                this.page += 1;
-                return data.hits;
-            }
+            this.page += 1;
+            return data.hits;
+     }catch(error) {
+         console.log("Что-то пошло не так при парсе.😡😡😡");  
+    }
+    } 
            
 
     resetPage() {
@@ -31,6 +40,5 @@ export default class newsApiService {
     set query(newQuery) {
         this.searchQuery = newQuery;
     }
-    
 
 }
